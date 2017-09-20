edb806ebb00e0e3c29a2f3312558edeb889fbcae
xsindira <soumya.indira@xpo.com>
2017-09-20 17:40:08 +0530

    DB data fetch code modified

--- LM_DMS2.0_UI_Automation/LM_DMS2.0_UI_Automation/LMAPI/Common/DBUtilities.cs
+++ LM_DMS2.0_UI_Automation/LM_DMS2.0_UI_Automation/LMAPI/Common/DBUtilities.cs
@@ -5,22 +5,68 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 namespace LM_DMS2._0_UI_Automation.LMAPI.Common
 {
     public class DBUtilities
     {
-        SqlConnection con = new SqlConnection("Data Source=10.54.48.53,3341;Initial Catalog=FC_MDT;User id=QASuperUser;Password=R@merT3@m_!;");
+       // string Connection = ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString;
+       // SqlConnection con = new SqlConnection("Data Source=10.54.48.53,3341;Initial Catalog=FC_MDT;User id=QASuperUser;Password=R@merT3@m_!;");
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString);
         //SqlCommand cmd;
         SqlDataAdapter adapt;
         public void openDBConnection()
         {
-            con.Open();
+            try
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("select top 10 * from fclegs", con);
+                adapt.Fill(dt);
+                con.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            //con.Open();
+            //SqlCommand cmd = new SqlCommand("select top 10 * from fclegs", con);
+            //cmd.CommandType = CommandType.Text;
+            //DataTable fillDt = new DataTable();
+            //SqlDataAdapter ad = new SqlDataAdapter();
+            //ad.SelectCommand = cmd;
+            //ad.Fill(fillDt);
+            //con.Close();
+        }
+
+        public DataTable fetchDatafromDb()
+        {
             DataTable dt = new DataTable();
-            adapt = new SqlDataAdapter("select top 10 * from fclegs", con);
-            adapt.Fill(dt);
-            //dataGridView1.DataSource = dt;
-            con.Close();
+            try
+            {
+                con.Open();
+                adapt = new SqlDataAdapter("select top 10 * from fclegs", con);
+                adapt.Fill(dt);
+                con.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return dt;
+            //con.Open();
+            //SqlCommand cmd = new SqlCommand("select top 10 * from fclegs", con);
+            //cmd.CommandType = CommandType.Text;
+            //DataTable fillDt = new DataTable();
+            //SqlDataAdapter ad = new SqlDataAdapter();
+            //ad.SelectCommand = cmd;
+            //ad.Fill(fillDt);
+            //con.Close();
         }
     }
 }
